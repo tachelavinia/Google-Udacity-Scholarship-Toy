7f6d6c64c6528a01b9ea9e7fba8c5b4b9335fffb
Lavinia Tache <lavinia.tachee@gmail.com>
2018-01-13 18:57:19 +0200

    T06.01-Exercise-SetupTheActivity - Completed

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,17 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowCustomEnabled(true);
+        }
+    }
+}
